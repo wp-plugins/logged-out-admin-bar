dfeabaa245a2522786299a1c6e4ab6db91503b88
westi <westi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-24 23:57:33 +0000

    Logged Out Admin Bar: Add support for displaying a registration link props sc0ttkclark.
    git-svn-id: https://plugins.svn.wordpress.org/logged-out-admin-bar/trunk@576889 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- logged-out-admin-bar.php
+++ logged-out-admin-bar.php
@@ -9,8 +9,11 @@
  */
 
 function pjw_logged_out_admin_bar( $wp_admin_bar) {
-	if ( !is_user_logged_in() ) 
+	if ( ! is_user_logged_in() ) {
 		$wp_admin_bar->add_menu( array( 'title' => __( 'Log In' ), 'href' => wp_login_url(), 'parent' => 'top-secondary' ) );
+		if ( get_option('users_can_register') )
+			$wp_admin_bar->add_menu( array( 'title' => __( 'Register' ), 'href' => site_url( 'wp-login.php?action=register' ), 'parent' => 'top-secondary' ) );
+	}
 }
 
 add_action( 'admin_bar_menu', 'pjw_logged_out_admin_bar' );
